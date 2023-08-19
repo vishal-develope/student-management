6d06872cb5d680912d62a38e2a748f604220d436
Vishal Patel <patel221007@gmail.com>
2023-08-19 13:51:21 +0530

    create api with token based authentication

--- main/views.py
+++ main/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from . import models
 from .serializer import TeacherSerializer
 from rest_framework.views import APIView
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework import status
 # Create your views here.
 
-class TeacherList(APIView):
-    def get(self, request):
-        teachers = models.Teacher.objects.all()
-        serializer = TeacherSerializer(teachers, many=True)
-        return Response(serializer.data)
+class TeacherList(generics.ListCreateAPIView):
+    queryset = models.Teacher.objects.all()
+    serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Teacher.objects.all()
+    serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
